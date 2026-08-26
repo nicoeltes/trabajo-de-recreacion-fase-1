59bc48118cee7bc8d72024391fd615d23ddfad75
nicoeltes <nicolaseltes@gmail.com>
2026-08-26 15:44:12 -0300

    cambios en toidos los css

---
+++ .js
@@ -0,0 +1,12 @@
+const esVegano = document.getElementById('vegano').checked;
+const esCeliaco = document.getElementById('celiaco').checked;
+const esVegetariano = document.getElementById('vegetariano').checked;
+
+// Al guardar el usuario:
+usuarios.push({ 
+    usuario, 
+    password, 
+    vegano: esVegano,
+    celiaco: esCeliaco,
+    vegetariano: esVegetariano
+});
